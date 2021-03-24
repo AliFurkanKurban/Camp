501d8201cc1779aa71d5aa1cf1ae923191bee6cf
furkan kurban <frkn_6898@hotmail.com>
2021-03-25 02:06:38 +0300

    3.Gün Ödev 3

---
+++ ClassMetotDemo/CostumerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CostumerManager
+    {
+        public void AddCostumer(Costumer costumer)
+        {
+            Console.WriteLine(costumer.Name + " " + costumer.Lastname + " "+"eklendi.");
+        }
+
+        public void DelCostumer(Costumer costumer)
+        {
+            Console.WriteLine(costumer.Name + " " + costumer.Lastname + " " + "silindi.");
+        }
+
+        public void MakeLists(Costumer costumer)
+        {
+            Costumer[] costumers = new Costumer[] { costumer };
+            Console.WriteLine(costumer.Name + " " + costumer.Lastname + " Listeye eklendi");
+
+            
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Costumer costumer1 = new Costumer();
+            costumer1.Name = "Ali";
+            costumer1.Lastname = "Kurban";
+            costumer1.Id = 12345;
+
+
+            Costumer costumer2 = new Costumer();
+            costumer2.Name = "Furkan";
+            costumer2.Lastname = "Kurban";
+            costumer2.Id = 67890;
+
+            CostumerManager costumermanager = new CostumerManager();
+            costumermanager.AddCostumer(costumer1);
+            costumermanager.MakeLists(costumer1);
+            costumermanager.DelCostumer(costumer1);
+    
+            Console.WriteLine("________________________________________");
+            costumermanager.AddCostumer(costumer2);
+            costumermanager.MakeLists(costumer2);
+            costumermanager.DelCostumer(costumer2);
+            
+
+
+
+        }
+    }
+}
