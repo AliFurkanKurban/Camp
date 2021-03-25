a7852ab89aa9b602f3907bc275e929ebf17419b7
furkan kurban <frkn_6898@hotmail.com>
2021-03-25 23:50:48 +0300

    4. Gün Sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic; 
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[] {"Furkan","Hasan","Emre","Ali" };
+
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+            //Console.WriteLine(names[2]);
+            //Console.WriteLine(names[3]);
+
+            //names[4] = "Veli";
+            //Console.WriteLine(names[4]);
+            //Bu şekilde yaparsak hata alırız
+
+            //names = new string[5];
+            //names[4] = "Veli";
+            //Console.WriteLine(names[4]);
+            //Bu şekilde yaparsak hata almayız ama 0 1 2 3 gibi başta kullandığımız değerleri kaybederiz.Bellekte yeni bir veri olarak tutulur
+            //Bu yüzden collections lar kullanılır
+
+            List<string> names2 = new List<string> { "Furkan", "Hasan", "Emre", "Ali" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[3]);
+
+            Console.WriteLine("-----------------------------------------");
+            names2.Add("Veli");
+            Console.WriteLine(names2[4]);
+            Console.WriteLine(names2[0]);
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string> ();
+            names.Add("Ahmet");
+            names.Add("Mehmet");
+            
+
+
+
+        }
+    }
+}
