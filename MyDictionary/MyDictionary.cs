3878578fa06611000741b580928fad3ad2e342c1
furkan kurban <frkn_6898@hotmail.com>
2021-03-26 15:47:23 +0300

    4. Gün Dictionary Oluşturma

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TypeKey,TypeValue>
+    {
+        TypeKey[] arrayKeys;
+        TypeValue[] arrayValues;
+        public MyDictionary()
+        {
+            arrayKeys = new TypeKey[0];
+            arrayValues = new TypeValue[0];
+        }
+
+        public void Add(TypeKey key ,TypeValue value)
+        {
+            TypeKey[] tempKeys;
+            TypeValue[] tempValues;
+
+            tempKeys = arrayKeys;
+            tempValues = arrayValues;
+
+            arrayKeys = new TypeKey[tempKeys.Length + 1];
+            arrayValues = new TypeValue[tempValues.Length + 1];
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                arrayKeys[i] = tempKeys[i];
+            }
+
+            for (int j = 0; j < tempValues.Length; j++)
+            {
+                arrayValues[j] = tempValues[j];
+            }
+
+
+            arrayKeys[arrayKeys.Length - 1] = key;
+            arrayValues[arrayValues.Length - 1] = value;
+        }
+
+        public int Count
+        {
+            get { return arrayKeys.Length; }
+            
+        }
+
+        public void ShowMe()
+        {
+
+            for (int x = 0; x < arrayKeys.Length; x++)
+            {
+                Console.WriteLine("Key : " + arrayKeys[x] + " = " + "Value : " + arrayValues[x]);
+
+            }
+        }
+        
+
+
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> sozluk = new MyDictionary<string, string>();
+            sozluk.Add("car", "araba");
+            sozluk.Add("flower", "çiçek");
+            sozluk.Add("dog", "köpek");
+
+            Console.WriteLine(sozluk.Count);
+            sozluk.ShowMe();
+        }
+    }
+}
